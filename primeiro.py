d672504091a65d7533fd646f18a2b370fb6f7872
Italo Gabriel <italogfx@gmail.com>
2022-08-16 20:06:22 -0300

    Primeiro Python

---
+++ primeiro.py
@@ -0,0 +1,20 @@
+import random
+def main():
+
+    smaller = int(input("Enter the smallen number: "))
+    larger = int(input("Enter the large number: "))
+    myNumber = random.randint(smaller, larger)
+    count = 0
+    while True:
+        count += 1
+        userNumber = int(input("Enter your guess: "))
+        if userNumber < myNumber:
+            print("Too small")
+        elif userNumber > myNumber:
+            print("Too large")
+        else:
+            print("You've got it in", count, "tries!")
+            break
+
+if __name__ == "__main__":
+    main()
